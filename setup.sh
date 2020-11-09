edb5064b8e5cfedb06226de9ef714f0ede7c6f71
Aman Tiwari <amantiwari@78632.com>
2020-11-09 23:15:27 +0530

    fixed bug

--- setup.sh
+++ setup.sh
@@ -1,16 +1,8 @@
-#! /bin/bash
-
-sudo su
-
-apt update
-
-apt install python3-pip
-
+sudo apt update -y
+sudo apt-get install python3-venv -y
+sudo apt-get install python3-pip -y
 python3 -m venv env
 source ./env/bin/activate
-
 pip3 install -r requirements.txt
-
 echo 'Make Env'
-
 echo 'Please run uvicorn --port 8000 --host 127.0.0.1 main:app --reload '
