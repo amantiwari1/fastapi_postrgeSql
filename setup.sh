a193cac28ddf151771439788d523f50bd42a75aa
Aman Tiwari <amantiwari@78632.com>
2020-11-09 23:28:40 +0530

    fixed bug

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt update -y
 sudo apt-get install python3-venv -y
 sudo apt-get install python3-pip -y
+sudo apt-get install python3-psycopg2 -y
 python3 -m venv env
 source ./env/bin/activate
 pip3 install -r requirements.txt
