b5cfd4073d928532fa2fa1e79fd2642f7049b9e7
Aman Tiwari <amantiwari@78632.com>
2020-11-10 02:57:15 +0530

    updated note

--- main.py
+++ main.py
@@ -66,29 +66,29 @@ async def shutdown():
 
 
 
-@app.get("/notes/", response_model=List[Note], status_code = status.HTTP_200_OK)
+@app.get("/note/", response_model=List[Note], status_code = status.HTTP_200_OK)
 async def read_notes(skip: int = 0, take: int = 20):
     query = notes.select().offset(skip).limit(take)
     return await database.fetch_all(query)
 
-@app.get("/notes/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
+@app.get("/note/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
 async def read_notes(note_id: int):
     query = notes.select().where(notes.c.id == note_id)
     return await database.fetch_one(query)
 
-@app.post("/notes/", response_model=Note, status_code = status.HTTP_201_CREATED)
+@app.post("/note/", response_model=Note, status_code = status.HTTP_201_CREATED)
 async def create_note(note: NoteIn):
     query = notes.insert().values(text=note.text, completed=note.completed)
     last_record_id = await database.execute(query)
     return {**note.dict(), "id": last_record_id}
 
-@app.put("/notes/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
+@app.put("/note/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
 async def update_note(note_id: int, payload: NoteIn):
     query = notes.update().where(notes.c.id == note_id).values(text=payload.text, completed=payload.completed)
     await database.execute(query)
     return {**payload.dict(), "id": note_id}
 
-@app.delete("/notes/{note_id}/", status_code = status.HTTP_200_OK)
+@app.delete("/note/{note_id}/", status_code = status.HTTP_200_OK)
 async def delete_note(note_id: int):
     query = notes.delete().where(notes.c.id == note_id)
     await database.execute(query)
