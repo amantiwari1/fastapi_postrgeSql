c863d28368f2bd7ac65e2a0b29173aafd3ac72a3
Aman Tiwari <amantiwari@78632.com>
2020-11-10 01:09:17 +0530

    remove files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update -y
-sudo apt-get install python3-venv -y
-sudo apt-get install python3-pip -y
-sudo apt-get install python3-psycopg2 -y
-python3 -m venv env
-source ./env/bin/activate
-pip3 install -r requirements.txt
-echo 'Make Env'
-echo 'Please run uvicorn --port 8000 --host 127.0.0.1 main:app --reload '
