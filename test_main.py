dee08940d520d05fd0d09983402fafadfe836503
Aman Tiwari <amantiwari@78632.com>
2020-11-10 13:23:25 +0530

    improved pytest

--- test_main.py
+++ test_main.py
@@ -5,7 +5,12 @@ from main import app
 client = TestClient(app)
 
 
+
 def test_read_main():
+    '''
+    If you want to build and push the docker image then type 'yes'
+    else type 'no'
+    '''
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {"msg": "yes"} # yes or no 
